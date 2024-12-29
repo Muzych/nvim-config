7cb752f6023f1a99988b30a0136d46cc0cd27b6d
Muzych <haxk1024@gmail.com>
2024-12-29 16:56:43 +0800

    remove scoop things

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -16,7 +16,14 @@ require("lazy").setup({
 	"williamboman/mason.nvim",
 	"williamboman/mason-lspconfig.nvim",
 	"neovim/nvim-lspconfig",
-
+    -- autocomplete
+    "hrsh7th/nvim-cmp",
+	"hrsh7th/vim-vsnip",
+	"hrsh7th/cmp-vsnip",
+	"hrsh7th/cmp-nvim-lsp",
+	"hrsh7th/cmp-buffer",
+	"hrsh7th/cmp-path",
+	"hrsh7th/cmp-cmdline",
     -- color scheme
     {
         "folke/tokyonight.nvim",
